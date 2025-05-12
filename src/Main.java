fcb1a00536fb250f032381ab89c6d03f73e31981
Костя <mr.xapt@mail.ru>
2025-05-12 18:28:04 +1000

    op2

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,14 @@ public class Main {
 
         System.out.println("Задание 1");
 
-        char color = '0';
-        switch (color) {
-            case '1':
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                break;
-            case '0':
-                System.out.println("Установите версию приложения для Android по ссылке");
-                break;
+        int clientOS = 0;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
+
         System.out.println("Задание 2");
 
         int clientOS2 = 0;
@@ -21,9 +20,8 @@ public class Main {
 
             if (clientOS2 == 0) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else if (clientOS2 == 1) {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                }
+            } else if (clientOS2 == 1)
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             if (clientOS2 == 0) {
                 System.out.println("Установите приложения для iOS по ссылке");
@@ -87,11 +85,8 @@ public class Main {
             case 11:
                 System.out.println("Осень");
                 break;
-            default:
-                System.out.println("Такого месяца нет");
         }
 
 
-
-        }
     }
+}
